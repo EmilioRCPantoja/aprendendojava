3ae12fb4bfc433af1a6c00b1dcfe8a289c691ff4
EmilioRCPantojaLnx <emiliorafael.pantoja02@gmail.com>
2025-08-15 11:59:50 -0300

    inicio da maratona java

---
+++ MaratonaJava/src/maratonajava/introduçao/Aula01.java
@@ -0,0 +1,10 @@
+
+package maratonajava.introduçao;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
---
+++ MaratonaJava/src/maratonajava/introduçao/Aula02TiposPrimitivos.java
@@ -0,0 +1,13 @@
+
+
+package maratonajava.introduçao;
+
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args)
+    {
+        int age = 10;
+        System.out.println(" the age is "+age+" years old");
+    }
+    
+}
