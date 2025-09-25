a990e9010658bb0f0d70de8359ee0b8cf06e0966
EmilioRCPantojaLnx <emiliorafael.pantoja46@gmail.com>
2025-09-24 22:04:09 -0300

    arrays de objetos

---
+++ MaratonaJava/src/maratonajava/poo/associacao/dominio/Jogador.java
@@ -0,0 +1,22 @@
+package maratonajava.poo.associacao.dominio;
+
+public class Jogador {
+    private String nome;
+
+    public void imprime() {
+        System.out.println(this.nome);
+    }
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
